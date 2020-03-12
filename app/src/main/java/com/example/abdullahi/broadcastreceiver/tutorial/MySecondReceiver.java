8d740db97e57289a4412e94b0fc4da5cf0c8d386
= <=>
2020-03-12 22:39:22 +0100

    Creating a broadcast with android action filters e.g Message, call etc

---
+++ app/src/main/java/com/example/abdullahi/broadcastreceiver/tutorial/MySecondReceiver.java
@@ -0,0 +1,12 @@
+package com.example.abdullahi.broadcastreceiver.tutorial;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MySecondReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
