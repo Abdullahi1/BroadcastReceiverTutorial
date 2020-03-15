5d147f1fa23afcee2d7e96fa46f85431d9be27dd
= <=>
2020-03-15 17:17:01 +0100

    Security Stage

--- app/src/main/java/com/example/abdullahi/broadcastreceiver/tutorial/MainActivity.java
+++ app/src/main/java/com/example/abdullahi/broadcastreceiver/tutorial/MainActivity.java
@@ -28,13 +28,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, MyFirstReceiver.class);
-//                sendBroadcast(intent);
-
-
-//                Intent intent = new Intent("android.mycustom.action");
-//                sendBroadcast(intent);
-
                 callFirstReceiver();
             }
         });
@@ -48,52 +41,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void callFirstReceiver(){
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-        Intent intent = registerReceiver(null, intentFilter);
-        
-        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        
-        if (status == BatteryManager.BATTERY_STATUS_CHARGING){
-            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -200);
-            Toast.makeText(this, "Battery Getting Charged\nLevel at "+level +"%", Toast.LENGTH_SHORT).show();
-
-        }else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING){
-            Toast.makeText(this, "Battery is Discharging", Toast.LENGTH_SHORT).show();
-        }else if (status ==  BatteryManager.BATTERY_STATUS_FULL){
-            Toast.makeText(this, "Battery Fully Charged", Toast.LENGTH_SHORT).show();
-        }
     }
 
     private void callThirdReceiver(){
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
-        registerReceiver(batteryStatusReceiver, intentFilter);
     }
 
-    private BroadcastReceiver batteryStatusReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-
-            int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-
-            if (status == BatteryManager.BATTERY_STATUS_CHARGING){
-                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -200);
-                Toast.makeText(context, "Battery Getting Charged\nLevel at "+level +"%", Toast.LENGTH_SHORT).show();
-
-            }else if (status == BatteryManager.BATTERY_STATUS_DISCHARGING){
-                Toast.makeText( context, "Battery is Discharging", Toast.LENGTH_SHORT).show();
-            }else if (status ==  BatteryManager.BATTERY_STATUS_FULL){
-                Toast.makeText(context, "Battery Fully Charged", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-    };
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        unregisterReceiver(batteryStatusReceiver);
-    }
 }
