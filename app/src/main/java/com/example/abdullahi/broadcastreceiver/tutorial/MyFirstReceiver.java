acfcf75b04b0d21324b2650bf05334149b6d81b9
= <=>
2020-03-15 04:53:52 +0100

    Staged for Dynamic Broadcast

--- app/src/main/java/com/example/abdullahi/broadcastreceiver/tutorial/MyFirstReceiver.java
+++ app/src/main/java/com/example/abdullahi/broadcastreceiver/tutorial/MyFirstReceiver.java
@@ -13,8 +13,6 @@ public class MyFirstReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "Hello from 1st receiver");
-        String name = intent.getStringExtra("name");
-        int age = intent.getIntExtra("age",0);
         Toast.makeText(context, "Hello from 1st receiver", Toast.LENGTH_LONG).show();
 
     }
